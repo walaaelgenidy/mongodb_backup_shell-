9084c7e746455a91d09139eae8a25cddfa843d47
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-09 17:28:40 +0200

    Update Mongo.sh

--- Mongo.sh
+++ Mongo.sh
@@ -6,28 +6,17 @@ TODAY=$(date +"%d%b%Y")
 DB_BACKUP_PATH='/backup/mongo'
 MONGO_HOST='localhost'
 MONGO_PORT='27017'
+DB_NAME = 'admin'
+MONGO_USER='DBUSER = 'root'
+MONGO_PASSWD='123456'
 
-MONGO_USER=''
-MONGO_PASSWD=''
-
-## Number of days to keep local backup copy
-BACKUP_RETAIN_DAYS=30   
- 
 mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 
 
-for DB_NAME in ${DATABASE_NAMES}
+for DB_NAME 
 	do
-		mongodump --host ${MONGO_HOST} --username ${MONGO_USER} --password ${MONGO_PASSWD} --port ${MONGO_PORT} --db ${DB_NAME}  --out ${DB_BACKUP_PATH}/${TODAY}/
+	mongodump --host="${MONGO_HOST}" --username="${MONGO_USER}" --password="${MONGO_PASSWD}" --port "${MONGO_PORT}" --db="${DB_NAME}"  --out="${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.bson"
 	done
+	
 
-######## Remove backups older than {BACKUP_RETAIN_DAYS} days  ########
- 
-DBDELDATE=$(date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago")
- 
-if [ ! -z ${DB_BACKUP_PATH} ]; then
-      cd ${DB_BACKUP_PATH}
-      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
-            rm -rf ${DBDELDATE}
-      fi
-fi
+
