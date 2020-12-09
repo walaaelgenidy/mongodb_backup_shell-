1589b13da091e6f1ea57d5a69a05dcfd619d9c69
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-09 17:43:57 +0200

    Update Mongo.sh

--- Mongo.sh
+++ Mongo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
- 
+
 export PATH=/bin:/usr/bin:/usr/local/bin
 TODAY=$(date +"%d%b%Y")
- 
+
 DB_BACKUP_PATH='/backup/mongo'
 MONGO_HOST='localhost'
 MONGO_PORT='27017'
-DB_NAME = 'admin'
-MONGO_USER='DBUSER = 'root'
+DB_NAME='admin'
+MONGO_USER='root'
 MONGO_PASSWD='123456'
 
-mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+sudo mkdir  ${DB_BACKUP_PATH}/${TODAY}
+
+for DB_NAME
+        do
+        mongodump --host="${MONGO_HOST}" --username="${MONGO_USER}" --password="${MONGO_PASSWD}" --port "${MONGO_PORT}" --db="${DB_NAME}"  --out="${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.bson"
+        done
 
 
-for DB_NAME 
-	do
-	mongodump --host="${MONGO_HOST}" --username="${MONGO_USER}" --password="${MONGO_PASSWD}" --port "${MONGO_PORT}" --db="${DB_NAME}"  --out="${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.bson"
-	done
-	
 
 
